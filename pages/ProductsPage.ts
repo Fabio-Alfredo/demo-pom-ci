31a3960824780e0dfd6b9cff0877c73bac3b297a
Fabio-Alfredo <00210121@uca.edu.sv>
2025-11-14 19:54:19 -0600

    :sparkles: feat: add new page

--- pages/ProductsPage.ts
+++ pages/ProductsPage.ts
@@ -6,7 +6,7 @@ export class ProductsPage {
 
   constructor(page: Page) {
     this.page = page;
-    this.title = page.locator('.title');
+    this.title = page.locator('span.title');
   }
 
   async verifyIsOnProductsPage() {
